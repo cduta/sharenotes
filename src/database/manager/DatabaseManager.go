2d2c28eb57b3105c975e03ffd8c537dc0e2c11e6
RyogaU <RyogaU@googlemail.com>
2016-01-01 21:59:22 +0100

    Formatting.

--- src/database/manager/DatabaseManager.go
+++ src/database/manager/DatabaseManager.go
@@ -1,249 +1,243 @@
 package manager
 
 import (
-    "database/sql"
+	"database/sql"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
-    "log"
+	"log"
+	"note"
 	"os"
-    "note"
-    "time"
-    "strconv"
+	"strconv"
+	"time"
 )
 
 const DB_FILE_NAME = "sndb.db"
 
-const INITIALIZE_NOTES_TABLE_EXEC = 
-    `create table notes (
+const INITIALIZE_NOTES_TABLE_EXEC = `create table notes (
         noteID integer not null primary key, 
         title text, 
         text text, 
         addDate DATETIME, 
         changeDate DATETIME
     );`
-    
-const SELECT_NOTES_QS = 
-    `select noteID, title, text, addDate, changeDate 
+
+const SELECT_NOTES_QS = `select noteID, title, text, addDate, changeDate 
      from notes`
-     
-const LOOKUP_NOTE_QS = 
-    `select title, text, addDate, changeDate 
+
+const LOOKUP_NOTE_QS = `select title, text, addDate, changeDate 
      from notes
      where noteID = ?`
-     
-const ADD_NOTE_EXEC = 
-    `insert into notes(title, text, addDate, changeDate) 
+
+const ADD_NOTE_EXEC = `insert into notes(title, text, addDate, changeDate) 
      values(?, ?, ?, ?);`
-     
-const UPDATE_NOTE_EXEC = 
-    `update notes 
+
+const UPDATE_NOTE_EXEC = `update notes 
      set title = ?, text = ?, changeDate = ?
      where noteID = ?;`
-     
-const DELETE_NOTE_EXEC = 
-    `delete from notes 
+
+const DELETE_NOTE_EXEC = `delete from notes 
      where noteID = ?;`
 
 type DatabaseManager struct {
-    notes []note.Note
-    db *sql.DB
+	notes []note.Note
+	db    *sql.DB
 }
 
-func New() DatabaseManager {    
-    dbm := DatabaseManager{}
-    
-    return dbm
+func New() DatabaseManager {
+	dbm := DatabaseManager{}
+
+	return dbm
 }
 
 func (dbm *DatabaseManager) Notes() []note.Note {
-    return dbm.notes
+	return dbm.notes
 }
 
 func (dbm *DatabaseManager) checkDBValidity() error {
-    rows, err := dbm.db.Query(SELECT_NOTES_QS)
-    
+	rows, err := dbm.db.Query(SELECT_NOTES_QS)
+
 	if err != nil {
 		log.Printf("%q: %s\n", err, "Rebuilding the database...")
-	} else {    
-        defer rows.Close()
-        for rows.Next() {
-            var noteID int
-            var title string
-            var text string
-            var addDate time.Time
-            var changeDate time.Time
-            rows.Scan(&noteID, &title, &text, &addDate, &changeDate)
-        }
-    }
-    
-    return err
+	} else {
+		defer rows.Close()
+		for rows.Next() {
+			var noteID int
+			var title string
+			var text string
+			var addDate time.Time
+			var changeDate time.Time
+			rows.Scan(&noteID, &title, &text, &addDate, &changeDate)
+		}
+	}
+
+	return err
 }
 
 func (dbm *DatabaseManager) initializeDB() error {
 	var err error
-    
-    os.Remove("./foo.db")
-    
-    dbm.db, err = sql.Open("sqlite3", "./"+DB_FILE_NAME)
-	
-    _, err = dbm.db.Exec(INITIALIZE_NOTES_TABLE_EXEC)
-	
-    if err != nil {
+
+	os.Remove("./foo.db")
+
+	dbm.db, err = sql.Open("sqlite3", "./"+DB_FILE_NAME)
+
+	_, err = dbm.db.Exec(INITIALIZE_NOTES_TABLE_EXEC)
+
+	if err != nil {
 		log.Printf("%q: %s\n", err, INITIALIZE_NOTES_TABLE_EXEC)
 	}
-	
-    return err
+
+	return err
 }
 
 func (dbm *DatabaseManager) Open() error {
-    var err error
-    
-    dbm.db, err = sql.Open("sqlite3", "./"+DB_FILE_NAME)
-    
+	var err error
+
+	dbm.db, err = sql.Open("sqlite3", "./"+DB_FILE_NAME)
+
 	if err != nil {
 		log.Fatal(err)
-        return err
+		return err
 	}
-    
-    if(dbm.checkDBValidity() != nil) {
-        dbm.Close()
-       	if err != nil {
-            log.Fatal(err)
-        } else {
-            dbm.initializeDB()
-        }
-    }
-    
-    return err
+
+	if dbm.checkDBValidity() != nil {
+		dbm.Close()
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			dbm.initializeDB()
+		}
+	}
+
+	return err
 }
 
 func (dbm *DatabaseManager) Close() {
-    dbm.db.Close()
+	dbm.db.Close()
 }
 
 func toSQLiteDateTimeString(t time.Time) string {
-    return fmt.Sprintf("", t.Year(), "-", t.Month(), "-", t.Day(), " ", t.Hour(), ":", t.Minute(), ":", t.Second())
+	return fmt.Sprintf("", t.Year(), "-", t.Month(), "-", t.Day(), " ", t.Hour(), ":", t.Minute(), ":", t.Second())
 }
 
 func (dbm *DatabaseManager) AddNote(n note.Note) error {
-    
-    transaction, err := dbm.db.Begin()
+
+	transaction, err := dbm.db.Begin()
 	if err != nil {
-        log.Printf("%q: %s\n", err, "Initializing add transaction.")
-        return err
+		log.Printf("%q: %s\n", err, "Initializing add transaction.")
+		return err
 	}
-	
-    stmt, err := transaction.Prepare(ADD_NOTE_EXEC)
+
+	stmt, err := transaction.Prepare(ADD_NOTE_EXEC)
 	if err != nil {
-        log.Printf("%q: %s\n", err, "Preparing add transaction.")
-        return err
+		log.Printf("%q: %s\n", err, "Preparing add transaction.")
+		return err
 	}
 	defer stmt.Close()
-	
-    _, err = stmt.Exec(n.Title(), n.Text(), toSQLiteDateTimeString(n.AddDate()), toSQLiteDateTimeString(n.ChangeDate()))
-    if err != nil {
-        log.Printf("%q: %s\n", err, "Add note in add transaction.")
-        return err
-    }
-    
+
+	_, err = stmt.Exec(n.Title(), n.Text(), toSQLiteDateTimeString(n.AddDate()), toSQLiteDateTimeString(n.ChangeDate()))
+	if err != nil {
+		log.Printf("%q: %s\n", err, "Add note in add transaction.")
+		return err
+	}
+
 	transaction.Commit()
-    
-    return err
+
+	return err
 }
 
 func (dbm *DatabaseManager) UpdateNote(n note.Note) error {
-    transaction, err := dbm.db.Begin()
+	transaction, err := dbm.db.Begin()
 	if err != nil {
-        log.Printf("%q: %s\n", err, "Initializing update transaction.")
-        return err
+		log.Printf("%q: %s\n", err, "Initializing update transaction.")
+		return err
 	}
-	
-    updateStatement, err := transaction.Prepare(UPDATE_NOTE_EXEC)
+
+	updateStatement, err := transaction.Prepare(UPDATE_NOTE_EXEC)
 	if err != nil {
-        log.Printf("%q: %s\n", err, "Preparing update transaction.")
-        return err
+		log.Printf("%q: %s\n", err, "Preparing update transaction.")
+		return err
 	}
 	defer updateStatement.Close()
-	
-    _, err = updateStatement.Exec(n.Title(), n.Text(), toSQLiteDateTimeString(n.ChangeDate()), strconv.Itoa(n.NoteID()))
-    if err != nil {
-        log.Printf("%q: %s\n", err, "Update note in update transaction.")
-        return err
-    }
-    
+
+	_, err = updateStatement.Exec(n.Title(), n.Text(), toSQLiteDateTimeString(n.ChangeDate()), strconv.Itoa(n.NoteID()))
+	if err != nil {
+		log.Printf("%q: %s\n", err, "Update note in update transaction.")
+		return err
+	}
+
 	transaction.Commit()
-    
-    return err
+
+	return err
 }
 
 func (dbm *DatabaseManager) DeleteNote(noteID int) error {
-    transaction, err := dbm.db.Begin()
+	transaction, err := dbm.db.Begin()
 	if err != nil {
-        log.Printf("%q: %s\n", err, "Initializing delete transaction.")
-        return err
+		log.Printf("%q: %s\n", err, "Initializing delete transaction.")
+		return err
 	}
-	
-    deleteStatement, err := transaction.Prepare(DELETE_NOTE_EXEC)
+
+	deleteStatement, err := transaction.Prepare(DELETE_NOTE_EXEC)
 	if err != nil {
-        log.Printf("%q: %s\n", err, "Preparing delete transaction.")
-        return err
+		log.Printf("%q: %s\n", err, "Preparing delete transaction.")
+		return err
 	}
 	defer deleteStatement.Close()
-	
-    _, err = deleteStatement.Exec(strconv.Itoa(noteID))
-    if err != nil {
-        log.Printf("%q: %s\n", err, "Update note in delete transaction.")
-        return err
-    }
-    
+
+	_, err = deleteStatement.Exec(strconv.Itoa(noteID))
+	if err != nil {
+		log.Printf("%q: %s\n", err, "Update note in delete transaction.")
+		return err
+	}
+
 	transaction.Commit()
-    
-    return err
+
+	return err
 }
 
 func (dbm *DatabaseManager) LoadNotes() ([]note.Note, error) {
-    dbm.notes = dbm.notes[0:0]
-    
-    rows, err := dbm.db.Query(SELECT_NOTES_QS)
-    
+	dbm.notes = dbm.notes[0:0]
+
+	rows, err := dbm.db.Query(SELECT_NOTES_QS)
+
 	if err != nil {
 		log.Printf("%q: %s\n", err, SELECT_NOTES_QS)
-	} else {    
-        defer rows.Close()
-        for rows.Next() {
-            var noteID int
-            var title string
-            var text string
-            var addDate time.Time
-            var changeDate time.Time
-            rows.Scan(&noteID, &title, &text, &addDate, &changeDate)
-            dbm.notes = append(dbm.notes, note.NewLocal(noteID, title, text, addDate, changeDate))
-        }
-    }
+	} else {
+		defer rows.Close()
+		for rows.Next() {
+			var noteID int
+			var title string
+			var text string
+			var addDate time.Time
+			var changeDate time.Time
+			rows.Scan(&noteID, &title, &text, &addDate, &changeDate)
+			dbm.notes = append(dbm.notes, note.NewLocal(noteID, title, text, addDate, changeDate))
+		}
+	}
 
-    return dbm.notes, err
+	return dbm.notes, err
 }
 
 func (dbm *DatabaseManager) GetNote(noteID int) (note.Note, error) {
-    
-    lookupQuery, err := dbm.db.Prepare(LOOKUP_NOTE_QS)
+
+	lookupQuery, err := dbm.db.Prepare(LOOKUP_NOTE_QS)
 	if err != nil {
 		log.Printf("%q: %s\n", err, LOOKUP_NOTE_QS)
-        return note.Note{}, err
+		return note.Note{}, err
 	}
-    
+
 	defer lookupQuery.Close()
 
-    var title string
-    var text string
-    var addDate time.Time
-    var changeDate time.Time
-    
+	var title string
+	var text string
+	var addDate time.Time
+	var changeDate time.Time
+
 	err = lookupQuery.QueryRow(strconv.Itoa(noteID)).Scan(&title, &text, &addDate, &changeDate)
 	if err != nil {
 		log.Printf("%q: %s\n", err, "Get Note scan failed.")
-        return note.Note{}, err
+		return note.Note{}, err
 	}
-    
-    return note.NewLocal(noteID, title, text, addDate, changeDate), err
+
+	return note.NewLocal(noteID, title, text, addDate, changeDate), err
 }
--- src/shareNotes.go
+++ src/shareNotes.go
@@ -1,100 +1,100 @@
 package main
 
 import (
-    "os/exec"
-    "fmt"
-    "html/template"
-    "net/http"
-    "regexp"
-    "strconv"
-    "database/manager"
-    "note"
-    "log"
-    "bytes"
-    "strings"
-    "github.com/mvdan/xurls"
+	"bytes"
+	"database/manager"
+	"fmt"
+	"github.com/mvdan/xurls"
+	"html/template"
+	"log"
+	"net/http"
+	"note"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type htmlTable struct {
-    Notes []htmlNote
+	Notes []htmlNote
 }
 
 type htmlNote struct {
-    NoteID int
-    Title string
-    Text template.HTML
+	NoteID int
+	Title  string
+	Text   template.HTML
 }
 
 func partialHtmlParser(text string) template.HTML {
-    var links []string = xurls.Relaxed.FindAllString(text, -1)
-    var newText string = ""
-    var restText string = text
-    
-    if len(links) > 0 {        
-        currentLink := links[0]
-        
-        for len(links) > 0 {
-            if strings.HasPrefix(restText, currentLink) {
-                newText = newText + "<a href=" + currentLink + ">" + currentLink + "</a>"
-                restText = restText[len(currentLink):len(restText)]
-                if len(links) > 1 {
-                    links = links[1:len(links)]
-                    currentLink = links[0]
-                } else if len(links) == 1 {
-                    currentLink = links[0]
-                    links = links[0:0]
-                } 
-            }
-            
-            if(len(restText) > 1) {
-                newText = newText + restText[0:1]
-                restText = restText[1:len(restText)]
-            } else if len(restText) == 1 {
-                newText = newText + restText[0:1]
-                restText = restText[0:0]
-            }
-        }
-    } else {
-        newText = restText
-    }
+	var links []string = xurls.Relaxed.FindAllString(text, -1)
+	var newText string = ""
+	var restText string = text
 
-    var lines []string = strings.Split(newText, "\n")
-    return template.HTML("<div>" + strings.Join(lines, "</div>\n<div>") + "</div>")
+	if len(links) > 0 {
+		currentLink := links[0]
+
+		for len(links) > 0 {
+			if strings.HasPrefix(restText, currentLink) {
+				newText = newText + "<a href=" + currentLink + ">" + currentLink + "</a>"
+				restText = restText[len(currentLink):len(restText)]
+				if len(links) > 1 {
+					links = links[1:len(links)]
+					currentLink = links[0]
+				} else if len(links) == 1 {
+					currentLink = links[0]
+					links = links[0:0]
+				}
+			}
+
+			if len(restText) > 1 {
+				newText = newText + restText[0:1]
+				restText = restText[1:len(restText)]
+			} else if len(restText) == 1 {
+				newText = newText + restText[0:1]
+				restText = restText[0:0]
+			}
+		}
+	} else {
+		newText = restText
+	}
+
+	var lines []string = strings.Split(newText, "\n")
+	return template.HTML("<div>" + strings.Join(lines, "</div>\n<div>") + "</div>")
 }
 
 func noteToHtmlNote(note note.Note) htmlNote {
-    return htmlNote{
-        NoteID: note.NoteID(),
-        Title: note.Title(),
-        Text: partialHtmlParser(note.Text())}
+	return htmlNote{
+		NoteID: note.NoteID(),
+		Title:  note.Title(),
+		Text:   partialHtmlParser(note.Text())}
 }
 
 var dbManager = manager.New()
 
 var templates = template.Must(template.ParseFiles("index.html", "AddNote.html", "Note.html", "DeleteNote.html", "PasteBinNote.html"))
 
-func indexHandler(writer http.ResponseWriter, request *http.Request) {    
-    var err error
-    var notes []note.Note
-    notes, err = dbManager.LoadNotes()
-    
-    if err != nil {
+func indexHandler(writer http.ResponseWriter, request *http.Request) {
+	var err error
+	var notes []note.Note
+	notes, err = dbManager.LoadNotes()
+
+	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
-    
-    var htmlNotes []htmlNote
-    
-    for _, n := range notes {
-        htmlNotes = append(htmlNotes, noteToHtmlNote(n))
-    }
-    
-    table := htmlTable{Notes : htmlNotes }
-    
+
+	var htmlNotes []htmlNote
+
+	for _, n := range notes {
+		htmlNotes = append(htmlNotes, noteToHtmlNote(n))
+	}
+
+	table := htmlTable{Notes: htmlNotes}
+
 	err = templates.ExecuteTemplate(writer, "index.html", table)
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
 }
 
@@ -102,150 +102,150 @@ func addNoteHandler(writer http.ResponseWriter, request *http.Request) {
 	err := templates.ExecuteTemplate(writer, "AddNote.html", note.Note{})
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
 }
 
 func newNoteHandler(writer http.ResponseWriter, request *http.Request) {
-    title := request.FormValue("title")
-    text := request.FormValue("text")
-    
-    err := dbManager.AddNote(note.New(title, text))
-    
-    if err != nil {
+	title := request.FormValue("title")
+	text := request.FormValue("text")
+
+	err := dbManager.AddNote(note.New(title, text))
+
+	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
-    
-    //fmt.Println("####\nAdd Note\n"+title+"\n"+text+"\n####")
-    
-    http.Redirect(writer, request, "/", http.StatusFound)
+
+	//fmt.Println("####\nAdd Note\n"+title+"\n"+text+"\n####")
+
+	http.Redirect(writer, request, "/", http.StatusFound)
 }
 
-func noteDetailsHandler(writer http.ResponseWriter, request *http.Request, noteID int) {    
-    var err error
-    var foundNote note.Note
-    
-    //fmt.Println("####\nGet Note "+strconv.Itoa(noteID)+"\n####")
+func noteDetailsHandler(writer http.ResponseWriter, request *http.Request, noteID int) {
+	var err error
+	var foundNote note.Note
 
-    foundNote, err = dbManager.GetNote(noteID)
-    if err != nil {
+	//fmt.Println("####\nGet Note "+strconv.Itoa(noteID)+"\n####")
+
+	foundNote, err = dbManager.GetNote(noteID)
+	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
-        
+
 	err = templates.ExecuteTemplate(writer, "Note.html", foundNote)
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
 }
 
 func saveNoteHandler(writer http.ResponseWriter, request *http.Request, noteID int) {
-    foundNote, err := dbManager.GetNote(noteID)
-    if err != nil {
+	foundNote, err := dbManager.GetNote(noteID)
+	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
-    
-    title := request.FormValue("title")
-    text := request.FormValue("text")
-    
-    //fmt.Println("####\nNote "+strconv.Itoa(noteID)+" saved\n"+title+"\n"+text+"\n####")
-    
-    var dirtyBit bool = false;
-    if(foundNote.Title() != title) {
-        dirtyBit = true;
-        foundNote.SetTitle(title)
-    }
-    
-    if(foundNote.Text() != text) {
-        dirtyBit = true;
-        foundNote.SetText(text);
-    }
-    
-    if(dirtyBit) {
-        err = dbManager.UpdateNote(foundNote)
-        if err != nil {
-            http.Error(writer, err.Error(), http.StatusInternalServerError)
-            return
-        }
-    }
-    
-    http.Redirect(writer, request, "/", http.StatusFound)
+
+	title := request.FormValue("title")
+	text := request.FormValue("text")
+
+	//fmt.Println("####\nNote "+strconv.Itoa(noteID)+" saved\n"+title+"\n"+text+"\n####")
+
+	var dirtyBit bool = false
+	if foundNote.Title() != title {
+		dirtyBit = true
+		foundNote.SetTitle(title)
+	}
+
+	if foundNote.Text() != text {
+		dirtyBit = true
+		foundNote.SetText(text)
+	}
+
+	if dirtyBit {
+		err = dbManager.UpdateNote(foundNote)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	http.Redirect(writer, request, "/", http.StatusFound)
 }
 
 func confirmDeleteNoteHandler(writer http.ResponseWriter, request *http.Request, noteID int) {
-    var err error
-    var foundNote note.Note
+	var err error
+	var foundNote note.Note
 
-    foundNote, err = dbManager.GetNote(noteID)
-    if err != nil {
+	foundNote, err = dbManager.GetNote(noteID)
+	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
-        
+
 	err = templates.ExecuteTemplate(writer, "DeleteNote.html", foundNote)
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
 }
 
 func deleteNoteHandler(writer http.ResponseWriter, request *http.Request, noteID int) {
-    err := dbManager.DeleteNote(noteID)
-    if err != nil {
-        http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    http.Redirect(writer, request, "/", http.StatusFound)
+	err := dbManager.DeleteNote(noteID)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(writer, request, "/", http.StatusFound)
 }
 
 func confirmPasteBinNoteHandler(writer http.ResponseWriter, request *http.Request, noteID int) {
-    var err error
-    var foundNote note.Note
+	var err error
+	var foundNote note.Note
 
-    foundNote, err = dbManager.GetNote(noteID)
-    if err != nil {
+	foundNote, err = dbManager.GetNote(noteID)
+	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
-        
+
 	err = templates.ExecuteTemplate(writer, "PasteBinNote.html", foundNote)
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
 }
 
 func pasteBinNoteHandler(writer http.ResponseWriter, request *http.Request, noteID int) {
-    var err error
-    var foundNote note.Note
+	var err error
+	var foundNote note.Note
 
-    foundNote, err = dbManager.GetNote(noteID)
-    if err != nil {
+	foundNote, err = dbManager.GetNote(noteID)
+	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
-    
-    shellCommand := exec.Command("curl", 
-        "-s", 
-        "-F", fmt.Sprintf("content=%s", foundNote.Text()), 
-        "-F", fmt.Sprintf("title=\"%s (ID:%d)\"",foundNote.Title(),foundNote.NoteID()), 
-        "http://dpaste.com/api/v2/")
-    
+
+	shellCommand := exec.Command("curl",
+		"-s",
+		"-F", fmt.Sprintf("content=%s", foundNote.Text()),
+		"-F", fmt.Sprintf("title=\"%s (ID:%d)\"", foundNote.Title(), foundNote.NoteID()),
+		"http://dpaste.com/api/v2/")
+
 	var output bytes.Buffer
 	shellCommand.Stdout = &output
 	err = shellCommand.Run()
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
-    
-    fmt.Println(output.String())
-    
-    http.Redirect(writer, request, output.String(), http.StatusFound)
+
+	fmt.Println(output.String())
+
+	http.Redirect(writer, request, output.String(), http.StatusFound)
 }
 
 var validPath = regexp.MustCompile("^/(Note|SaveNote|ConfirmDeleteNote|DeleteNote|PasteBinNote|ConfirmPasteBinNote)/([0-9]+)$")
@@ -257,32 +257,32 @@ func makeNoteIDHandler(function func(http.ResponseWriter, *http.Request, int)) h
 			http.NotFound(writer, request)
 			return
 		}
-        id, _ := strconv.Atoi(urlTokens[2])
+		id, _ := strconv.Atoi(urlTokens[2])
 		function(writer, request, id)
 	}
 }
 
 func main() {
-    http.HandleFunc("/", indexHandler)
-    http.HandleFunc("/AddNote/", addNoteHandler)
-    http.HandleFunc("/NewNote/", newNoteHandler)
-    http.HandleFunc("/Note/", makeNoteIDHandler(noteDetailsHandler))
-    http.HandleFunc("/SaveNote/", makeNoteIDHandler(saveNoteHandler))
-    http.HandleFunc("/ConfirmDeleteNote/", makeNoteIDHandler(confirmDeleteNoteHandler))
-    http.HandleFunc("/DeleteNote/", makeNoteIDHandler(deleteNoteHandler))
-    http.HandleFunc("/ConfirmPasteBinNote/", makeNoteIDHandler(confirmPasteBinNoteHandler))
-    http.HandleFunc("/PasteBinNote/", makeNoteIDHandler(pasteBinNoteHandler))
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/AddNote/", addNoteHandler)
+	http.HandleFunc("/NewNote/", newNoteHandler)
+	http.HandleFunc("/Note/", makeNoteIDHandler(noteDetailsHandler))
+	http.HandleFunc("/SaveNote/", makeNoteIDHandler(saveNoteHandler))
+	http.HandleFunc("/ConfirmDeleteNote/", makeNoteIDHandler(confirmDeleteNoteHandler))
+	http.HandleFunc("/DeleteNote/", makeNoteIDHandler(deleteNoteHandler))
+	http.HandleFunc("/ConfirmPasteBinNote/", makeNoteIDHandler(confirmPasteBinNoteHandler))
+	http.HandleFunc("/PasteBinNote/", makeNoteIDHandler(pasteBinNoteHandler))
 
-    err := dbManager.Open()
-    
-    if err != nil {
-        log.Fatal(err)
-        return 
+	err := dbManager.Open()
+
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
-    
-    defer dbManager.Close()
 
-    log.Printf("ShareNotes initialized...")
+	defer dbManager.Close()
+
+	log.Printf("ShareNotes initialized...")
 
 	http.ListenAndServe(":8080", nil)
 }
